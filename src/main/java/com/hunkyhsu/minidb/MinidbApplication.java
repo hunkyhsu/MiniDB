10678e819dd8e272fe1cf86b9fc42ae85d15b056
hunkyhsu <hunkyhsu0801@gmail.com>
2026-01-16 18:14:45 +0800

    init

---
+++ src/main/java/com/hunkyhsu/minidb/MinidbApplication.java
@@ -0,0 +1,13 @@
+package com.hunkyhsu.minidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hunkyhsu/minidb/MinidbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunkyhsu.minidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
